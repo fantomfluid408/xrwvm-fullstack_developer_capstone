0f31042646f3347e8a1d9e8e7ffb0bbbcaaa79dc
Bei <xhubei@gmail.com>
2024-12-15 14:48:32 -0500

    lint 1

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,6 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -16,9 +15,9 @@ class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     # Other fields as needed
+
     def __str__(self):
         return self.name  # Return the name as the string representation
-
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
